738340991c08eb9466ea9903e1fad4885801771f
Victor Onofiok <radoncreep@gmail.com>
2020-07-31 16:49:04 +0100

    actions_file

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -3,11 +3,12 @@ import { connect } from 'react-redux'
 
 import CounterControl from '../../components/CounterControl/CounterControl';
 import CounterOutput from '../../components/CounterOutput/CounterOutput';
+import * as actionTypes from '../../store/actions';
 
 class Counter extends Component {
-    state = {
-        counter: 0
-    }
+    // state = {
+    //     counter: 0
+    // } not using this approach here
 
     counterChangedHandler = ( action, value ) => {
         switch ( action ) {
@@ -24,7 +25,7 @@ class Counter extends Component {
                 this.setState( ( prevState ) => { return { counter: prevState.counter - value } } )
                 break;
         }
-    }
+    };
 
     render () {
         return (
@@ -34,6 +35,13 @@ class Counter extends Component {
                 <CounterControl label="Decrement" clicked={this.props.onDecrementCounter}  />
                 <CounterControl label="Add 10" clicked={this.props.onAddCounter}  />
                 <CounterControl label="Subtract 15" clicked={this.props.onSubtractCounter}  />
+                <hr/>
+                <button onClick={this.props.onStoreResult}>Store Result</button>
+                <ul>
+                    {this.props.counterResult.map(storedResult => {
+                      return <li key={storedResult.id} onClick={() => this.props.onDeleteResult(storedResult.id)}>{storedResult.value}</li> 
+                    })}
+                </ul>
             </div>
         );
     }
@@ -41,16 +49,20 @@ class Counter extends Component {
 
 const mapStateToProps = state => {
     return {
-        ctr: state.counter
+        ctr: state.counter,
+        counterResult: state.results
     };
 };
 
+// You can dispatch actions that dont get used in the reducer
 const mapDispatchToProps = dispatch => { // takes in dispatch which is a helper function
-    return {
-        onIncrementCounter: () => dispatch({ type: 'INCREMENT', val: 1 }),
-        onDecrementCounter: () => dispatch({ type: 'DECREMENT', value: 1 }),
-        onAddCounter: () => dispatch({ type: 'ADD' , value: 10 }), // value could be used inside a payload obj but here it is a payload itself, an addtional data passed into action
-        onSubtractCounter: () => dispatch({ type: 'SUBTRACT', value: 15 })
+    return { // these obj properties are available to the props object of this container because mapDispatchToProps function is connected to the container using th connect function
+        onIncrementCounter: () => dispatch({ type: actionTypes.INCREMENT, val: 1 }), 
+        onDecrementCounter: () => dispatch({ type: actionTypes.DECREMENT, value: 1 }),
+        onAddCounter: () => dispatch({ type: actionTypes.ADD , value: 10 }), // value could be used inside a payload obj but here it is a payload itself, an addtional data passed into action
+        onSubtractCounter: () => dispatch({ type: actionTypes.SUBTRACT, value: 15 }),
+        onStoreResult: () => dispatch({ type: actionTypes.STORE_RESULT }),
+        onDeleteResult: (id) => dispatch({ type: actionTypes.DELETE_RESULT, resultId: id })
     };
 };
 
@@ -58,7 +70,7 @@ export default connect(mapStateToProps, mapDispatchToProps)(Counter);
 
 // when the component element is clicked 
 // the function available to props is fired or gets executed
-// it is available to props because the connect function connects mapDispatch  function to the container
+// it is available to props because the connect function connects mapDispatch function to the container
 // so the function returns this object and it's proprerties as properties to the container's prop object
 // so when a prop in the mapDispatch gets executed which depends on the component element clicked on
 // because each hold distinct reference to properties on mapToDispatch
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,28 +1,52 @@
-import React from 'react';
+import * as actionTypes from './actions';
 
 const initialState = {
-    counter: 0
+    counter: 0,
+    results: []
 };
 
 const reducer = (state = initialState, action) => { // action arg is a object that receives an action from the dispatc method
     // Using switch statements instead of multiple ifs
 
     switch (action.type) {
-        case 'INCREMENT': 
+        case actionTypes.INCREMENT: 
+            // another approach to immutably update an existing state object
+            const newState = Object.assign({}, state);
+            newState.counter = state.counter + 1;
+            return newState;
+           // the "break" keyword isnt needed bcos of the return keyword
+        case actionTypes.DECREMENT: 
             return {
-                counter: state.counter + action.val
-            }; // the break keyword isnt needed bcos of the return keyword
-        case 'DECREMENT': 
-            return {
+                ...state,
                 counter: state.counter - action.value // this is just like accessing counter using setState
             };
-        case ('ADD'): 
+        case actionTypes.ADD: 
             return {
+                ...state,
                 counter: state.counter + action.value
             };
-        case ('SUBTRACT'):
+        case actionTypes.SUBTRACT:
             return {
+                ...state,
                 counter: state.counter - action.value
+            };
+        case actionTypes.STORE_RESULT:
+            console.log(action.type)
+            return {
+                ...state,
+                results: state.results.concat({id: new Date(), value: state.counter}) // so the counter value is being stored in the array or concatenated each time it is being updated
+            };
+        case actionTypes.DELETE_RESULT:
+            // 1st approach to updating an array immutably 
+            // const id = action.resultId;
+            // const newArray = [...state.result];
+            // newArray.splice(id, 1)
+
+            // Second approach
+            const updateArray = state.results.filter(result => result.id !== action.resultId) // if no equal then it should be passed into the new array and returned at the end of the operation
+            return {
+                ...state,
+                results: updateArray
             };
     };
 
@@ -54,3 +78,16 @@ export default reducer;
 //         counter: state.counter - action.value
 //     };
 // };
+
+
+// What the spread operator does 
+// Tells javascript to return a JS object
+// Take all the properties and values of the state argument "...state" which is our old state
+// Distribute these props with their values in this new object
+// and then since we define an additional property, add this property to the object
+// or if it already was present due to us distributing the old state
+// overwrite this and only this
+// that's how we should update the old state immutably
+// dont touch the old state like let newState = oldstate
+// dont just return a new obj without covering all the properties of the old state
+// or you are about to delete props from the old state
